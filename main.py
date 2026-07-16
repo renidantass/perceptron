cd520a9102f22cf79ebf595cf74ca98f524d778a
Reni Dantas <renidantas0@gmail.com>
2026-07-15 22:50:24 -0300

    fix: consertando lógica

--- main.py
+++ main.py
@@ -19,39 +19,45 @@ class TrainingData:
 
 
 class Perceptron:
-    def __init__(self, learning_rate: float) -> None:
-        self.learning_rate = learning_rate
-        self.bias = 0
-        self.weights = []
-        self.max_epochs = 100
+    def __init__(self, inputs_length: int, learning_rate: float) -> None:
+        self.inputs_length = inputs_length
+        self.learning_rate: float = learning_rate
+        self.bias: float = 0.0
+        self.weights: list[float] = [0.0] * self.inputs_length
+        self.max_epochs: int = 100
 
-    def train(self, data: TrainingData):
-        for epoch in range(0, self.max_epochs):
+    def train(self, data: TrainingData) -> None:
+        for _ in range(0, self.max_epochs):
             epoch_errors = 0
             for sample in data:
                 predicted = self.infer(sample.inputs)
 
                 error = sample.expected_output - predicted
 
-                # TODO: Implementar
-                self.update_weights(sample, error)
-                self.update_bias(error)
+                self._update_parameters(sample, error)
 
-                if error > 0:
+                if error != 0:
                     epoch_errors += 1
 
-            if epoch_errors <= 0:
+            if epoch_errors == 0:
                 break
 
-    def update_weights(self, sample: Sample, error: float):
+    def _update_parameters(self, sample: Sample, error: float) -> None:
+        self._update_weights(sample, error)
+        self._update_bias(error)
+
+    def _update_weights(self, sample: Sample, error: float) -> None:
         for i in range(0, len(sample.inputs)):
             self.weights[i] += self.learning_rate * error * sample.inputs[i]
 
-    def update_bias(self, error: float):
+    def _update_bias(self, error: float) -> None:
         self.bias += error * self.learning_rate
 
     def infer(self, inputs: list[int]) -> int:
         total = 0
+
+        if len(inputs) != self.inputs_length:
+            raise ValueError("Número inválido de entradas")
 
         for i in range(0, len(inputs)):
             total += inputs[i] * self.weights[i]
@@ -60,8 +66,8 @@ class Perceptron:
 
         if total >= 0:
             return 1
-        
-        return 0
+        else:
+            return 0
 
 def main():
     dataset = TrainingData()
@@ -73,13 +79,12 @@ def main():
     dataset.add(Sample([0, 1, 0], 0))
     dataset.add(Sample([0, 0, 1], 0))
 
-    perceptron = Perceptron(learning_rate=0.02)
+    perceptron = Perceptron(3, learning_rate=0.02)
     perceptron.train(dataset)
 
-    output = perceptron.infer([1,0,1])
+    output = perceptron.infer([1,1,1])
 
-    print(f"Saída: {output}")
-
+    print("Acertou a previsão" if output == 1 else "Errou a previsão")
 
 
 if __name__ == '__main__':
